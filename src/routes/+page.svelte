d6010f7ac75c816925722853b1babb6c4db6bc9f
Michal <hi@geodev.me>
2024-01-12 20:53:17 +0100

    style: add icon as svelte component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,17 @@
-<h1 class="text-3xl font-bold underline">
-  Hello my new website!
-</h1>
+<script>
+  import Globe from '$components/icons/globe.svelte';
+</script>
+
+<div class="flex flex-col items-center w-100 mt-12">
+  <div class="flex items-center">
+    <h1 class="text-primaryBlue text-5xl">GEO</h1>
+    <div class="mx-4">
+      <Globe width={120} stroke={'#1400FF'} />
+    </div>
+    <h1 class="text-primaryBlue text-5xl">DEV</h1>
+  </div>
+  <div class="mt-4">
+    <p class="text-primaryBlue">programming & cartography</p>
+  </div>
+</div>
+
