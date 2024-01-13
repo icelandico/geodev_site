71b999c3ed862930b5df12161c473ccbe152887b
Michal <hi@geodev.me>
2024-01-13 23:11:01 +0100

    style: change main font

--- src/lib/components/icons/globe.svelte
+++ src/lib/components/icons/globe.svelte
@@ -1,6 +1,21 @@
 <script>
-  export let width = 24;
-  export let stroke = '#000'
-  export let strokeWidth = 1
+	export let width = 24;
+	export let stroke = '#000';
+	export let strokeWidth = 1;
 </script>
-<svg xmlns="http://www.w3.org/2000/svg" viewbox={`0 0 24 24`} width={width} height={width} fill="none" stroke={stroke} stroke-width={strokeWidth} stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe"><circle cx="12" cy="12" r="10"/><path d="M2 12h20M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"/></svg>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox={`0 0 24 24`}
+	{width}
+	height={width}
+	fill="none"
+	{stroke}
+	stroke-width={strokeWidth}
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class="feather feather-globe"
+	><circle cx="12" cy="12" r="10" /><path
+		d="M2 12h20M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"
+	/></svg
+>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,21 @@
 <script>
-  import "../app.css";
-  import "@fontsource/overpass";
-  import NavLink from '$components/NavLink.svelte';
+	import '../app.css';
+	import '@fontsource-variable/aleo';
+	import NavLink from '$components/NavLink.svelte';
 </script>
 
 <header>
-  <nav>
-    <div class="flex items-center gap-4 px-6 h-16">
-      <NavLink text='home' route='/' />
-      <NavLink text='works' route='/works' />
-      <NavLink text='writing' route='/blog' />
-      <NavLink text='reading' route='/reading' />
-      <NavLink text='about' route='/about' styles='ml-auto' />
-    </div>
-  </nav>
+	<nav>
+		<div class="flex items-center gap-4 px-6 h-16">
+			<NavLink text="home" route="/" />
+			<NavLink text="works" route="/works" />
+			<NavLink text="writing" route="/blog" />
+			<NavLink text="reading" route="/books" />
+			<NavLink text="about" route="/about" styles="ml-auto" />
+		</div>
+	</nav>
 </header>
 
-<slot />
+<main class="mt-16">
+	<slot />
+</main>
