908e4b8e864dfa9f8afaaddbbb6d638532eff352
Michal <hi@geodev.me>
2024-01-13 18:31:37 +0100

    chore: add mdsvex, config svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.svelte', '.md', '.svx']
+		})
+	],
+	extensions: ['.svelte', '.md'],
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -23,7 +29,8 @@ const config = {
 			split: false
 		}),
 		alias: {
-			$components: 'src/lib/components'
+			$components: 'src/lib/components',
+			$utils: 'src/lib/utils'
 		}
 	}
 };
