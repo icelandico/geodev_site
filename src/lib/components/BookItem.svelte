b465f45831fa29171c11d4ddc03c48c9d3370f98
Michal <hi@geodev.me>
2024-01-13 20:08:04 +0100

    fix: clean up BookItem component

--- src/lib/components/BookItem.svelte
+++ src/lib/components/BookItem.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  import Rating from '$components/Rating.svelte';
-  export let title: string = '';
-  export let date: string = '';
-  export let author: string = '';
-  export let rating: string = '';
-
-  console.log('rating', rating)
+	import Rating from '$components/Rating.svelte';
+	export let title: string = '';
+	export let date: string = '';
+	export let author: string = '';
+	export let rating: string = '';
 </script>
 
-<div class="flex my-4">
-  <div class="basis-10/12">
-    <h3 class="text-3xl text-primaryBlack text-bold italic">{title}</h3>
-    <h2 class="text-2xl text-primaryBlack text-bold my-4">{author}</h2>
-    <h2 class="text-2xl text-primaryBlack">{date}</h2>
-  </div>
-  <Rating rating={parseFloat(rating)} style="grow"/>
+<div class="flex mb-12 cursor-pointer group">
+	<div class="basis-10/12">
+		<h3 class="text-3xl text-primaryBlack font-bold italic group-hover:text-primaryBlue">
+			{title}
+		</h3>
+		<h2 class="text-2xl text-primaryBlack font-bold my-4 group-hover:text-primaryBlue">{author}</h2>
+		<h2 class="text-2xl text-primaryBlack group-hover:text-primaryBlue">{date}</h2>
+	</div>
+	<Rating rating={parseFloat(rating)} style="grow" />
 </div>
