f36c2c9117911cbc0dada483918de826bbc046b0
Michal <hi@geodev.me>
2024-01-13 18:31:16 +0100

    chore: add color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		colors: {
-			primaryBlue: '#1400FF'
+			primaryBlue: '#1400FF',
+			primaryBlack: '#3E3E3E'
 		},
 		extend: {}
 	},
