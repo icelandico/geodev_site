cb8691788cb1ad38bc72f28303aa016cbcfe1fa0
Michal <hi@geodev.me>
2024-01-21 17:30:30 +0100

    style: add responsive styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 	<nav>
 		<div class="flex items-center gap-4 px-6 h-16">
 			<NavLink text="home" route="/" />
-			<NavLink text="works" route="/works" />
+			<NavLink text="projects" route="/projects" />
 			<NavLink text="writing" route="/blog" />
 			<NavLink text="reading" route="/books" />
 			<NavLink text="about" route="/about" styles="ml-auto" />
@@ -16,6 +16,6 @@
 	</nav>
 </header>
 
-<main class="mt-16">
+<main class="mt-12">
 	<slot />
 </main>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,5 +5,11 @@
 
 <TitleBar title="About" />
 <Wrapper>
-	<h1>About me</h1>
+	<div>
+		<p>Hello</p>
+		<p>This site is built with SvelteKit. For content management I use DecapCMS.</p>
+		<p>
+			<a href="mailto: hi@geodev.me">Email: hi@geodev.me</a>
+		</p>
+	</div>
 </Wrapper>
