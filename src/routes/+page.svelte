03cc3ea2e0c95dc7db64728a8d3a76b1c48bc806
Michal <hi@geodev.me>
2024-01-21 19:10:13 +0100

    style: fix style on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
   <div class="flex items-center">
     <h1 class="text-primaryBlue text-5xl">GEO</h1>
     <div class="mx-4">
-      <Globe width={120} stroke={'#1400FF'} />
+      <Globe width={120} stroke={'#2563EB'} />
     </div>
     <h1 class="text-primaryBlue text-5xl">DEV</h1>
   </div>
