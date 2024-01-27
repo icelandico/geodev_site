3c69a46e8ad1302f15ea9c563ea8ee2a4aa5f9c1
Michal <hi@geodev.me>
2024-01-27 22:52:38 +0100

    feature: Add footer with icons

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Github from './icons/github.svelte';
+	import Kofi from './icons/kofi.svelte';
+	import Mastodon from './icons/mastodon.svelte';
+</script>
+
+<footer class="flex justify-center items-center p-4 mt-auto gap-8">
+  <a href="https://github.com/icelandico">
+  	<Github />
+  </a>
+  <a href="https://fosstodon.org/@muszynov" target="_blank">
+  	<Mastodon />
+  </a>
+  <a href="https://ko-fi.com/geodev" target="_blank">
+  	<Kofi />
+  </a>
+  <a href="https://www.geodev.me/rss.xml">RSS</a>
+</footer>
