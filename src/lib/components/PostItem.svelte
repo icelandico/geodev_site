c5fdee71e3a3f3ae50c78c157d5229a00b31dd52
Michal <hi@geodev.me>
2024-02-11 16:12:21 +0100

    style: fix styling for post item

--- src/lib/components/PostItem.svelte
+++ src/lib/components/PostItem.svelte
@@ -2,17 +2,15 @@
 	import type { Post } from '$lib/server/blogPosts';
 	import { formatDate } from '$utils/formatDate';
 	export let post: Post;
-  export let withYear = false;
+	export let withYear = false;
 </script>
 
-<div
-	class="flex gap-4 dark:text-white justify-between items-center hover:outline-1 hover:outline-secondaryBlue hover:outline-dashed py-3"
+<a
+	class="flex justify-between items-center group gap-4 dark:text-white hover:outline-1 hover:outline-secondaryBlue hover:outline-dashed py-3 cursor-pointer
+  gap-4 text-xl font-semibold hover:dark:text-secondaryBlue hover:text-primaryBlue"
+	href={`blog/${post.slug}`}
 >
-	<a
-		class="text-xl font-semibold hover:dark:text-secondaryBlue hover:text-primaryBlue"
-		href={`blog/${post.slug}`}
+	{post.title}
+	<time class="text-sm text-right basis-3/12 ml-auto italic">{formatDate(post.date, withYear)}</time
 	>
-		{post.title}
-	</a>
-	<time class="text-sm text-right basis-3/12 ml-auto italic">{formatDate(post.date, withYear)}</time>
-</div>
+</a>
