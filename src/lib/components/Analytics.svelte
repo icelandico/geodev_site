b2d916ff23ee2312a9e232244c36ec13a1d256e2
Michal <hi@geodev.me>
2024-02-14 20:41:25 +0100

    fix: refactor analytics component

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,21 +1,20 @@
-<script>
+<script lang="ts">
 	import { PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID } from '$env/static/public';
+	import { browser } from '$app/environment';
+
+	if (browser) {
+		window.dataLayer = window.dataLayer || [];
+		window.gtag = function gtag(): void {
+			window.dataLayer.push(arguments);
+		};
+		window.gtag('js', new Date());
+		window.gtag('config', PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID);
+	}
 </script>
 
 <svelte:head>
 	<script
 		async
-		src={`https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}`}
-	>
-	</script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-
-		gtag('js', new Date());
-		gtag('config', PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID);
-	</script>
+		src="https://www.googletagmanager.com/gtag/js?id={PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}"
+	></script>
 </svelte:head>
