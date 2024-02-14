59673bd4e76b5da360fb494826fc3d6d50c32857
Michal <hi@geodev.me>
2024-02-14 18:50:00 +0100

    style: add overflow scroll if needed

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -4,21 +4,22 @@
 	import Wrapper from '$components/Wrapper.svelte';
 	import { formatDate } from '$utils/formatDate';
 	export let data;
-	const doubledData = data.booksStats.concat(data.booksStats);
 </script>
 
 <TitleBar title="Reading" />
 <Wrapper>
-	<div class="flex flex-col justify-center pt-4">
-		<div class="flex items-center gap-8 mb-8">
-			{#each data.booksStats as stat}
-				<div class="flex flex-col items-center gap-1/2 md:gap-1">
-					<div class="w-4 h-4 bg-primaryBlue dark:bg-primaryOrange rounded-full mb-1" />
-					<p class="font-bold dark:text-white">{stat.year}</p>
-					<p class="whitespace-nowrap dark:text-white">Pages: {stat.pages}</p>
-					<p class="dark:text-white">Books: {stat.books}</p>
-				</div>
-			{/each}
+	<div class="flex flex-col justify-center pt-4 overflow-auto">
+		<div>
+			<div class="flex items-center gap-8 mb-8 overflow-auto">
+				{#each data.booksStats as stat}
+					<div class="flex flex-col items-center gap-1/2 md:gap-1">
+						<div class="w-4 h-4 bg-primaryBlue dark:bg-primaryOrange rounded-full mb-1" />
+						<p class="font-bold dark:text-white">{stat.year}</p>
+						<p class="whitespace-nowrap dark:text-white">Pages: {stat.pages}</p>
+						<p class="dark:text-white">Books: {stat.books}</p>
+					</div>
+				{/each}
+			</div>
 		</div>
 		<div>
 			{#each data.books as book}
