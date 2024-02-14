2fc6137e67a4147b4abe8704f1e5f7ddc3b0a562
Michal <hi@geodev.me>
2024-02-14 18:56:01 +0100

    chore: change svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -23,7 +23,7 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
-			precompress: false,
+			precompress: true,
 			strict: true,
 			edge: false,
 			split: false
