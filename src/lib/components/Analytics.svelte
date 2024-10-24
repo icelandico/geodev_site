029898b957efee2c90d3f2bc33f588bba889f35c
Michal Muszynski <hi@geodev.me>
2024-10-24 18:57:31 +0200

    feature: handle lazy loading gtm script

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,21 +1,32 @@
 <script lang="ts">
 	import { PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID } from '$env/static/public';
 	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 
-	if (browser) {
-		window.dataLayer = window.dataLayer || [];
-		window.gtag = function gtag(): void {
-			window.dataLayer.push(arguments);
+	const loadGoogleAnalytics = () => {
+		const script = document.createElement('script');
+		script.src = `https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}`;
+		script.async = true;
+		
+		script.onload = () => {
+			window.dataLayer = window.dataLayer || [];
+			window.gtag = function gtag(): void {
+					window.dataLayer.push(arguments);
+			};
+			window.gtag('js', new Date());
+			window.gtag('config', PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID);
 		};
-		window.gtag('js', new Date());
-		window.gtag('config', PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID);
-	}
-</script>
 
-<svelte:head>
-	<script
-		async
-		defer
-		src="https://www.googletagmanager.com/gtag/js?id={PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}"
-	></script>
-</svelte:head>
+		document.head.appendChild(script);
+	};
+
+	onMount(() => {
+		if (browser) {
+			if ('requestIdleCallback' in window) {
+					window.requestIdleCallback(() => loadGoogleAnalytics());
+			} else {
+					setTimeout(loadGoogleAnalytics, 3000);
+			}
+		}
+	});
+</script>
