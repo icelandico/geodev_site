3276d04688580107554dc10c8bb99e2773b2b976
Michal Muszynski <hi@geodev.me>
2024-11-02 21:28:30 +0100

    style: set conditiona icon stroke

--- src/lib/components/icons/externalLink.svelte
+++ src/lib/components/icons/externalLink.svelte
@@ -4,6 +4,6 @@
 	export let color = '#3E3E3E';
 </script>
 
-<svg fill="none" height="24" width="24">
-  <path d="M10 6H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-4M14 4h6m0 0v6m0-6L10 14" stroke="{color}" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+<svg fill="none" height="24" width="24" class="stroke-primaryBlack dark:stroke-white">
+  <path d="M10 6H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-4M14 4h6m0 0v6m0-6L10 14" stroke-width="2"/>
 </svg>
