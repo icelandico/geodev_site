eb3cb216c41c3a02114001994a0bb1a2588f2c9d
Michal Muszynski <hi@geodev.me>
2024-11-04 19:51:47 +0100

    Shorten meta description

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,4 +1,4 @@
 export const PAGE_TITLE = 'Programming and cartography';
 export const PAGE_DESCRIPTION =
-	"Software developer sharing insights on TypeScript, React, web development and cartography. Here you can find articles and my works related to programming and cartography";
+	"Software developer sharing insights on TypeScript, React, web development and cartography.";
 
