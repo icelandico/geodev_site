0085ed06ba281a655dc7376dbe9c5f56ac672809
Michal Muszynski <hi@geodev.me>
2024-11-04 19:46:38 +0100

    change meta description

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,3 +1,4 @@
 export const PAGE_TITLE = 'Programming and cartography';
 export const PAGE_DESCRIPTION =
-	"Hello, I'm Michal, front-end developer from Poland. Specialized in JavaScript, Typescript, React and React Native. Welcome on my website where you can find articles and my works related to programming and cartography";
+	"Software developer sharing insights on TypeScript, React, web development and cartography. Here you can find articles and my works related to programming and cartography";
+
