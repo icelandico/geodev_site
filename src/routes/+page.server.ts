08bfdedff86a6a539debf54d5ec7dbe6b1b954c7
Michal Muszynski <hi@geodev.me>
2025-08-23 21:24:54 +0200

    add projects to main page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,6 @@
 import { posts } from '$lib/server/blogPosts';
 import { books } from '$lib/server/books';
+import { projects } from '$lib/server/projects';
 import { PAGE_DESCRIPTION } from '$utils/constants';
 import type { PageServerLoad } from './$types';
 
@@ -8,6 +9,7 @@ export const load: PageServerLoad = async () => {
 		title: 'Programming and cartography',
 		description: PAGE_DESCRIPTION,
 		posts: posts.slice(0, 5),
-		books: books.slice(0, 5)
+		books: books.slice(0, 5),
+		projects: projects.slice(0, 5)
 	};
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,9 +13,9 @@
 			class="text-primaryBlue dark:text-white text-5xl flex items-center gap-4"
 			aria-label="GEODEV"
 		>
-		<span aria-hidden="true">
-			<Globe width={75} style="stroke-primaryBlue dark:stroke-white" />
-		</span>
+			<span aria-hidden="true">
+				<Globe width={75} style="stroke-primaryBlue dark:stroke-white" />
+			</span>
 			geodev
 		</h1>
 	</div>
@@ -23,14 +23,27 @@
 		<p class="text-primaryBlue dark:text-white">programming & cartography</p>
 	</div>
 	<Wrapper contentStyle="flex-col mt-12">
-		<h2 class="text-primaryBlack dark:text-white text-4xl">Recent posts</h2>
-		<div class="mt-8 mb-8">
+		<a href="/blog/">
+			<h2
+				class="text-primaryBlack dark:text-white text-4xl text-primaryBlack dark:text-white text-4xl hover:dark:text-primaryOrange hover:text-primaryBlue"
+			>
+				Recent posts
+			</h2></a
+		>
+		<div class="mt-8 mb-8 border-b">
 			{#each data.posts as post}
 				<PostItem {post} withYear />
 			{/each}
 		</div>
-		<h2 class="text-primaryBlack dark:text-white text-4xl">Reading</h2>
-		<div class="mt-8">
+
+		<a href="/books/"
+			><h2
+				class="text-primaryBlack dark:text-white text-4xl hover:dark:text-primaryOrange hover:text-primaryBlue"
+			>
+				Reading
+			</h2></a
+		>
+		<div class="mt-8 mb-8 border-b">
 			{#each data.books as book}
 				<a href={`books/${book.slug}`} role="button" title="book review">
 					<BookItem
@@ -39,6 +52,28 @@
 						date={formatDate(book.date)}
 						rating={book.rating}
 					/>
+				</a>
+			{/each}
+		</div>
+
+		<a href="/projects/"
+			><h2
+				class="text-primaryBlack dark:text-white text-4xl text-primaryBlack dark:text-white text-4xl hover:dark:text-primaryOrange hover:text-primaryBlue"
+			>
+				Projects
+			</h2></a
+		>
+		<div class="mt-8 mb-8">
+			{#each data.projects as project}
+				<a
+					class="flex justify-between items-center group gap-4 dark:text-white hover:outline-1 hover:outline-primaryBlue hover:dark:outline-primaryOrange hover:outline-dashed py-3 cursor-pointer
+  gap-4 text-xl font-semibold hover:dark:text-primaryOrange hover:text-primaryBlue"
+					href={`projects/${project.slug}`}
+				>
+					{project.title} &#8226; {project.type}
+					<time class="text-sm text-right basis-3/12 ml-auto italic"
+						>{formatDate(project?.date, true)}</time
+					>
 				</a>
 			{/each}
 		</div>
