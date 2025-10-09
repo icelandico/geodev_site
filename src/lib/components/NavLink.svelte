7c5c5c3dc81484f45bf97f3ebc11aab8041f23d7
Michal Muszynski <hi@geodev.me>
2025-10-09 21:09:28 +0200

    feat: set hover on link

--- src/lib/components/NavLink.svelte
+++ src/lib/components/NavLink.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <a
-	class={`relative text-primaryBlack dark:text-white text-xl font-semibold hover:dark:text-primaryOrange ${
+	class={`relative text-primaryBlack dark:text-white text-xl font-semibold hover:text-primaryBlue hover:dark:text-primaryOrange ${
 		active
 			? 'text-primaryBlue after:absolute after:border-b-4 after:border-b-primaryBlue after:dark:border-b-primaryOrange after:right-0 after:left-0 after:-bottom-2'
 			: ''
