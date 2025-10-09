4ab277b72032e4048c0f1ab3eed38c4f4f182d1d
Michal Muszynski <hi@geodev.me>
2025-10-09 18:19:35 +0200

    style: title fix

--- src/lib/components/TitleBar.svelte
+++ src/lib/components/TitleBar.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class="flex items-center justify-center w-full md:w-5/6 bg-primaryBlue dark:bg-white h-16">
-	<h1 class="text-white dark:text-primaryBlack text-xl md:text-3xl">{title}</h1>
+	<h1 class="text-white dark:text-primaryBlack text-xl md:text-3xl mx-4 md:mx-0">{title}</h1>
 </div>
