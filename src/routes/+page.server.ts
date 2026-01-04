03a7e034c07f64065c98b4263045e234b38a8039
Michal Muszynski <hi@geodev.me>
2026-01-04 17:54:14 +0100

    feat: update title

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,12 @@
 import { posts } from '$lib/server/blogPosts';
 import { books } from '$lib/server/books';
 import { projects } from '$lib/server/projects';
-import { PAGE_DESCRIPTION } from '$utils/constants';
+import { PAGE_DESCRIPTION, PAGE_TITLE } from '$utils/constants';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	return {
-		title: 'Programming and cartography',
+		title: PAGE_TITLE,
 		description: PAGE_DESCRIPTION,
 		posts: posts.slice(0, 5),
 		books: books.slice(0, 5),
