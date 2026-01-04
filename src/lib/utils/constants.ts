0ed62efcb1131783e1a48009461df14641d9bb79
Michal Muszynski <hi@geodev.me>
2026-01-04 17:51:40 +0100

    feat: meta tags change

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,3 +1,2 @@
-export const PAGE_TITLE = 'Programming and cartography';
-export const PAGE_DESCRIPTION =
-	'A blog exploring the intersection of cartography and web development with insights on React, Svelte, TypeScript, and more.';
+export const PAGE_TITLE = 'geodev.me';
+export const PAGE_DESCRIPTION = 'Web development, cartography and my other output.';
