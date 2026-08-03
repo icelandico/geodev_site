0f0cb2bad94cacc96226920a3fcb95a7050c00fc
Michal Muszynski <hi@geodev.me>
2026-08-03 18:23:32 +0200

    feat: show only projects with published flag

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -11,7 +11,7 @@
 <Wrapper contentStyle="lg:w-7/12">
 	<div class="flex md:flex-row justify-between w-full mt-8">
 		<div class="grid grid-cols md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
-			{#each data.projects as project}
+			{#each data.projects.filter((project) => project.published) as project}
 				<a href={project.selfUrl ? project.selfUrl : `/projects/${project.slug}`}>
 					<ProjectCard data={project} type={project.type} />
 				</a>
