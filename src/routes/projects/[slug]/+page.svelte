19b64822b061a662d7d31e7225b7866f4e1bdc7d
Michal Muszynski <hi@geodev.me>
2026-08-03 18:40:31 +0200

    feat: add a guard for project that is not published

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -10,37 +10,41 @@
 
 <Wrapper contentStyle="md:w-4/6">
 	<div class="gap-8 w-full mt-4">
-		<p class="text-white italic">Published: {getMonthWithYear(data.created)}</p>
-		<article>
-			<div class="dynamic-content">
-				<svelte:component this={data.content} />
+		{#if data.meta.published === true}
+			<p class="text-white italic">Published: {getMonthWithYear(data.created)}</p>
+			<article>
+				<div class="dynamic-content">
+					<svelte:component this={data.content} />
+				</div>
+			</article>
+			<div class="flex flex-col mb-auto">
+				<a href={data.meta.image} target="_blank" class="block mb-4 h-3/6">
+					<img
+						src={data.meta.image}
+						alt="project"
+						class="hover:grayscale-0 grayscale mx-auto md:w-4/6 shadow-xl"
+					/>
+				</a>
+				<span class="text-sm italic dark:text-white">Click to open in full size</span>
+				<div class="mt-6 flex gap-6">
+					{#if data.meta.url}
+						<a
+							class="text-primaryBlue hover:bg-primaryBlue dark:text-primaryOrange dark:hover:text-white hover:text-white inline-block underline hover:decoration-white hover:decoration-2 dark:hover:bg-primaryOrange"
+							target="_blank"
+							href={data.meta.url}>Project</a
+						>
+					{/if}
+					{#if data.meta.repoUrl}
+						<a
+							class="text-primaryBlue hover:bg-primaryBlue dark:text-primaryOrange dark:hover:text-white hover:text-white inline-block underline hover:decoration-white hover:decoration-2 dark:hover:bg-primaryOrange"
+							target="_blank"
+							href={data.meta.repoUrl}>Repository</a
+						>
+					{/if}
+				</div>
 			</div>
-		</article>
-		<div class="flex flex-col mb-auto">
-			<a href={data.meta.image} target="_blank" class="block mb-4 h-3/6">
-				<img
-					src={data.meta.image}
-					alt="project"
-					class="hover:grayscale-0 grayscale mx-auto md:w-4/6 shadow-xl"
-				/>
-			</a>
-			<span class="text-sm italic dark:text-white">Click to open in full size</span>
-			<div class="mt-6 flex gap-6">
-				{#if data.meta.url}
-					<a
-						class="text-primaryBlue hover:bg-primaryBlue dark:text-primaryOrange dark:hover:text-white hover:text-white inline-block underline hover:decoration-white hover:decoration-2 dark:hover:bg-primaryOrange"
-						target="_blank"
-						href={data.meta.url}>Project</a
-					>
-				{/if}
-				{#if data.meta.repoUrl}
-					<a
-						class="text-primaryBlue hover:bg-primaryBlue dark:text-primaryOrange dark:hover:text-white hover:text-white inline-block underline hover:decoration-white hover:decoration-2 dark:hover:bg-primaryOrange"
-						target="_blank"
-						href={data.meta.repoUrl}>Repository</a
-					>
-				{/if}
-			</div>
-		</div>
+		{:else}
+			<p class="text-center text-lg text-gray-700 dark:text-gray-200">Page not accessible</p>
+		{/if}
 	</div>
 </Wrapper>
